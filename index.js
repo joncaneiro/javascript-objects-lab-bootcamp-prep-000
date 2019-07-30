a4128edfe0ac5356a2c1631feaf1f292ff0a6e39
Jon Caneiro <joncaneiro@gmail.com>
2019-07-30 00:57:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newerRecipes = Object.assign({}, object)
-  delete newerRecipes.
+  delete newerRecipes[key]
 }
