b764b50861a3b2b4e36e783599db1fea0121fafc
Jon Caneiro <joncaneiro@gmail.com>
2019-07-30 01:24:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const recipes = {milk: '1 cup', flour: '4 cups'}
 
 
 function updateObjectWithKeyValue (object, key, value) {
-  newRecipe = Object.assign({}, object, {[key]:value})
-  return newRecipe
+  return Object.assign({}, object, {[key]:value})
 }
 
 
